44bb266edddd4d648dec3a3f831807e97ccd609f
Abubakr Amjad <143190037+MianAbubakr@users.noreply.github.com>
2025-01-28 17:19:31 +0500

    SignIn/SignUp Flow - Implement Google Map

--- app/src/main/java/com/smlab/zapride/ui/signUp/SignUp.java
+++ app/src/main/java/com/smlab/zapride/ui/signUp/SignUp.java
@@ -1,5 +1,6 @@
 package com.smlab.zapride.ui.signUp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,18 +10,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.smlab.zapride.R;
+import com.smlab.zapride.databinding.ActivitySignUpBinding;
+import com.smlab.zapride.ui.signIn.SignIn;
 
 public class SignUp extends AppCompatActivity {
+    ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setListener();
+    }
+
+    private void setListener() {
+        binding.backBtn.setOnClickListener(view -> onBackPressed());
+
+        binding.signUpButton.setOnClickListener(view -> {
+           startActivity(new Intent(this, SignUpOTPVerification.class));
+        });
+
+        binding.textViewSignIn.setOnClickListener(view -> {
+            startActivity(new Intent(this, SignIn.class));
         });
     }
 }
