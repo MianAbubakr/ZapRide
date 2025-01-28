12ef11b8ff3b8f4d51777736bb37fcaf6b609bf2
syedalinaqvi1965 <syedalimaqvi1965@gmail.com>
2025-01-28 20:19:29 +0500

    notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     // Country Code Picker
     implementation ("com.github.vardemin.MaterialCountryPicker:materialcountrypicker:0.2.3")
     // Google Map
-    implementation ("com.google.android.gms:play-services-maps:18.1.0")
-    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation ("com.google.android.gms:play-services-maps:19.0.0")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
 
 
 
