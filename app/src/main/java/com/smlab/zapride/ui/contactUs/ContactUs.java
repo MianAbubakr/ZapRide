81fb82a145ce3a6471437b42122af7c7e2b82a98
Abubakr Amjad <143190037+MianAbubakr@users.noreply.github.com>
2025-02-03 18:59:50 +0500

    Privacy Policy - Contact Us - About Us - Edit Profile

---
+++ app/src/main/java/com/smlab/zapride/ui/contactUs/ContactUs.java
@@ -0,0 +1,36 @@
+package com.smlab.zapride.ui.contactUs;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.smlab.zapride.R;
+import com.smlab.zapride.databinding.ActivityContactUsBinding;
+
+public class ContactUs extends AppCompatActivity {
+    ActivityContactUsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityContactUsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setListener();
+    }
+
+    private void setListener() {
+        binding.backBtn.setOnClickListener(v -> {
+            onBackPressed();
+        });
+    }
+}
--- app/src/main/java/com/smlab/zapride/ui/deleteAccount/DeleteAccount.java
+++ app/src/main/java/com/smlab/zapride/ui/deleteAccount/DeleteAccount.java
@@ -9,18 +9,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.smlab.zapride.R;
+import com.smlab.zapride.databinding.ActivityDeleteAccountBinding;
 
 public class DeleteAccount extends AppCompatActivity {
+    ActivityDeleteAccountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_delete_account);
+        binding = ActivityDeleteAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setListener();
+    }
+
+    private void setListener() {
+        binding.backBtn.setOnClickListener(v -> {
+            onBackPressed();
         });
     }
 }
